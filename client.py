fbd849180a441a89b4f7343bb8b23fa66a157475
robotanh <robotanh15@gmail.com>
2024-04-04 14:54:40 +0700

    update client.py

--- client.py
+++ client.py
@@ -5,10 +5,14 @@ class Client:
     def __init__(self, host, port):
         self.host = host
         self.port = port
+        self.peer_port = 12346  # Use a different port for peer-to-peer communication
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    def connect(self):
-        self.socket.connect((self.host, self.port))
+    def connect(self, host=None, port=None):
+        if host is None and port is None:
+            host, port = self.host, self.port
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect((host, port))
 
     def send_message(self, message):
         self.socket.send(message.encode('ascii'))
@@ -20,15 +24,36 @@ class Client:
         self.socket.close()
 
     def get_filenames_in_folder(self):
-        
         current_dir = os.path.dirname(os.path.abspath(__file__))
-        folder_path = os.path.join(current_dir, 'file')
-        
-        # List all files in the 'file' directory
+        folder_path = os.path.join(current_dir, 'file_split')
         filenames = os.listdir(folder_path)
-        # Filter out directories, keep file names
         filenames = [f for f in filenames if os.path.isfile(os.path.join(folder_path, f))]
         return filenames
+
+    def serve_file_piece(self):
+        # Setup server to send file piece to other client
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind(('0.0.0.0', self.peer_port))
+            s.listen()
+            print("Serving file pieces on port:", self.peer_port)
+            conn, addr = s.accept()
+            with conn:
+                print('Connected by', addr)
+                while True:
+                    data = conn.recv(1024)
+                    if not data:
+                        break
+                    # Assume data is the filename to send
+                    filename = data.decode('ascii')
+                    file_path = os.path.join('file_split', filename)
+                    if os.path.exists(file_path):
+                        with open(file_path, 'rb') as f:
+                            piece = f.read()
+                            conn.sendall(piece)
+                    else:
+                        conn.sendall(b'File not found')
+                print("Finished serving file piece.")
+
     def send_filenames_to_server(self):
         filenames = self.get_filenames_in_folder()
         if not filenames:
@@ -36,33 +61,22 @@ class Client:
             return
 
         filenames_str = '\n'.join(filenames)
-        while True:
-            print("Sent list of filenames to server.")
-            print(filenames_str)
-            #send
-            self.send_message(filenames_str)
-            received_message = self.receive_message()
-            print('Received from the server:', received_message)
-            ans = input('\nDo you want to continue(y/n): ')
-            if ans != 'y':
-                break
-    
-    def start_communication(self):
-        message = "shaurya says "
-        while True:
-            self.send_message(message)
-            received_message = self.receive_message()
-            print('Received from the server:', received_message)
-            ans = input('\nDo you want to continue(y/n): ')
-            if ans != 'y':
-                break
+        print("Sent list of filenames to server.")
+        print(filenames_str)
+        self.send_message(filenames_str)
+        received_message = self.receive_message()
+        print('Received from the server:', received_message)
+
+        if 'Others client want to download piece' in received_message:
+            self.close()  # Close connection with server
+            self.serve_file_piece()  # Serve file piece to other client
+            # Optionally, reconnect to main server after serving
+            # self.connect()
 
 if __name__ == '__main__':
     host = '192.168.1.3'
     port = 12345
     client = Client(host, port)
     client.connect()
-    #connect to sever
-
     client.send_filenames_to_server()
     client.close()
